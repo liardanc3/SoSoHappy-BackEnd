460a50b1b137b50d72405718fbf9e7e14c33b8c0
liardanc3 <liardanc3@gmail.com>
2023-08-05 17:18:37 +0900

    [Add] dm-service 프로젝트 추가 (#2)

---
+++ dm-service/src/test/java/sosohappy/dmservice/DmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sosohappy.dmservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
