dda24a7fceadbaf12c9b30a52b059a2211293b6b
liardanc3 <liardanc3@gmail.com>
2023-08-26 22:42:52 +0900

    [Feat] accessToken, refreshToken (#5)

--- auth-service/src/main/java/sosohappy/authservice/oauth2/handler/OAuth2LoginSuccessHandler.java
+++ auth-service/src/main/java/sosohappy/authservice/oauth2/handler/OAuth2LoginSuccessHandler.java
@@ -1,15 +1,11 @@
 package sosohappy.authservice.oauth2.handler;
 
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
-import sosohappy.authservice.entity.User;
-import sosohappy.authservice.repository.UserRepository;
 import sosohappy.authservice.jwt.service.JwtService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import jakarta.servlet.ServletException;
@@ -23,24 +19,20 @@ import java.io.IOException;
 public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
     private final JwtService jwtService;
-    private final UserRepository userRepository;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         DefaultOAuth2User oAuth2User = (DefaultOAuth2User) authentication.getPrincipal();
 
-        System.out.println("oAuth2User.getAuthorities() = " + oAuth2User.getAuthorities());
-        System.out.println("oAuth2User.getAttributes() = " + oAuth2User.getAttributes());
-
         String accessToken = jwtService.createAccessToken(oAuth2User.getName());
+        String refreshToken = jwtService.createRefreshToken();
 
         response.addHeader(jwtService.getAccessHeader(), "Bearer " + accessToken);
 
-        jwtService.sendAccessAndRefreshToken(response, accessToken, null);
+        jwtService.setAccessTokenAndRefreshTokenOnHeader(response, accessToken, null);
         loginSuccess(response, oAuth2User);
     }
 
-    // TODO : 소셜 로그인 시에도 무조건 토큰 생성하지 말고 JWT 인증 필터처럼 RefreshToken 유/무에 따라 다르게 처리해보기
     private void loginSuccess(HttpServletResponse response, DefaultOAuth2User oAuth2User) throws IOException {
         System.out.println("OAuth2LoginSuccessHandler.loginSuccess");
         String accessToken = jwtService.createAccessToken(oAuth2User.getName());
@@ -48,7 +40,7 @@ public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
         response.addHeader(jwtService.getAccessHeader(), "Bearer " + accessToken);
         response.addHeader(jwtService.getRefreshHeader(), "Bearer " + refreshToken);
 
-        jwtService.sendAccessAndRefreshToken(response, accessToken, refreshToken);
+        jwtService.setAccessTokenAndRefreshTokenOnHeader(response, accessToken, refreshToken);
         jwtService.updateRefreshToken(oAuth2User.getName(), refreshToken);
     }
 }
--- auth-service/src/main/java/sosohappy/authservice/oauth2/service/CustomOAuth2UserService.java
+++ auth-service/src/main/java/sosohappy/authservice/oauth2/service/CustomOAuth2UserService.java
@@ -1,7 +1,6 @@
 package sosohappy.authservice.oauth2.service;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
@@ -10,26 +9,18 @@ import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import sosohappy.authservice.entity.User;
 import sosohappy.authservice.oauth2.OAuthAttributes;
-import sosohappy.authservice.repository.UserRepository;
 
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
-
-    private final UserRepository userRepository;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        System.out.println("CustomOAuth2UserService.loadUser");
         OAuth2User oAuth2User = new DefaultOAuth2UserService().loadUser(userRequest);
 
         String provider = userRequest.getClientRegistration().getRegistrationId();
