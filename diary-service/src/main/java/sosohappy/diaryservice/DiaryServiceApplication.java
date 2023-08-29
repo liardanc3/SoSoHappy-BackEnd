044ab9c86085cbe4127be41d7cc0965bc785bb5f
liardanc3 <liardanc3@gmail.com>
2023-08-29 15:27:02 +0900

    [Add] init diary-service project (#6)

---
+++ diary-service/src/main/java/sosohappy/diaryservice/DiaryServiceApplication.java
@@ -0,0 +1,13 @@
+package sosohappy.diaryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaryServiceApplication.class, args);
+	}
+
+}
