854b2a08594180a64d25e76afbfbc6dde24bfb62
liardanc3 <liardanc3@gmail.com>
2023-09-08 05:23:17 +0900

    [Fix] kafka (de)serializer (#3)

--- dm-service/src/main/java/sosohappy/dmservice/kafka/KafkaConsumer.java
+++ dm-service/src/main/java/sosohappy/dmservice/kafka/KafkaConsumer.java
@@ -5,6 +5,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
@@ -13,9 +14,9 @@ public class KafkaConsumer {
 
     private final ConcurrentHashMap<String, String> emailAndTokenMap;
 
-    @KafkaListener(topics = "accessToken", groupId = "ASddd2ddsafdaddddasdd")
+    @KafkaListener(topics = "accessToken", groupId = "ASddd2ddsafdaddjdasdd")
     public void addAccessToken(ConsumerRecord<String, String> record){
-        String email = record.key();
+        String email = String.valueOf(record.key());
         String accessToken = record.value();
 
         emailAndTokenMap.put(email, accessToken);
