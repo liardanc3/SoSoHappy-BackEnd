15970a071995312599747e5d33f6b10a09994e0f
liardanc3 <liardanc3@gmail.com>
2023-09-10 04:26:29 +0900

    [Add] kafka producer (#6)

---
+++ feed-service/src/main/java/sosohappy/feedservice/kafka/KafkaProducerAspect.java
@@ -0,0 +1,23 @@
+package sosohappy.feedservice.kafka;
+
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class KafkaProducerAspect {
+
+    private final KafkaTemplate<byte[], byte[]> kafkaTemplate;
+
+    @AfterReturning(value = "@annotation(kafkaProducer)", returning = "result")
+    public void handleKafkaProducer(JoinPoint joinPoint, KafkaProducer kafkaProducer, Object result){
+        String email = (String) joinPoint.getArgs()[0];
+        kafkaTemplate.send(kafkaProducer.topic(), email.getBytes(), ((String) result).getBytes());
+    }
+
+}
