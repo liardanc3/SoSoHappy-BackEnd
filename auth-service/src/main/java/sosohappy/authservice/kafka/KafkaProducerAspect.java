5faeb757d23ec4917f43ad479cf66a50154397bf
liardanc3 <liardanc3@gmail.com>
2023-10-05 18:37:31 +0900

    [Feat] kafka topic expired (#5)

--- auth-service/src/main/java/sosohappy/authservice/kafka/KafkaProducerAspect.java
+++ auth-service/src/main/java/sosohappy/authservice/kafka/KafkaProducerAspect.java
@@ -7,12 +7,17 @@ import org.aspectj.lang.annotation.Aspect;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 @Aspect
 @Component
 @RequiredArgsConstructor
 public class KafkaProducerAspect {
 
     private final KafkaTemplate<byte[], byte[]> kafkaTemplate;
+    private final ScheduledExecutorService scheduledExecutorService;
 
     @AfterReturning(value = "@annotation(kafkaProducer)", returning = "result")
     public void handleKafkaProducer(JoinPoint joinPoint, KafkaProducer kafkaProducer, Object result){
@@ -20,6 +25,14 @@ public class KafkaProducerAspect {
         if(kafkaProducer.topic().equals("accessToken")){
             String email = (String) joinPoint.getArgs()[0];
             kafkaTemplate.send(kafkaProducer.topic(), email.getBytes(), ((String) result).getBytes());
+
+            scheduledExecutorService.schedule(
+                    () -> {
+                        kafkaTemplate.send("expired", email.getBytes(), null);
+                    },
+                    36000000,
+                    TimeUnit.MILLISECONDS
+            );
         }
 
         if(kafkaProducer.topic().equals("resign")){
