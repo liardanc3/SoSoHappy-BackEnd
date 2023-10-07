98316bfe177d13313b1935894dcb131e59cd5eaf
liardanc3 <liardanc3@gmail.com>
2023-10-08 00:38:50 +0900

    [Feat] test api (#1)

---
+++ config-service/src/main/java/sosohappy/configservice/TestController.java
@@ -0,0 +1,13 @@
+package sosohappy.configservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("test-actuator")
+    public String test(){
+        return "config-service on";
+    }
+}
